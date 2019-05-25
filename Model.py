01eafac9b3541f8a4b413ec31aee113a8cd1250d
anubhav13@gmail.com <Evolution2019!>
2019-05-24 22:12:13 -0700

    First commit

---
+++ Model.py
@@ -0,0 +1,13 @@
+import DataGenerator
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+
+
+class Model():
+
+    def __init__(self):
+
+
+
+    def createData(self):
