16c4b1ae2c3568e9d54d91d4308c5e4d33a36e0c
anubhav13@gmail.com <Evolution2019!>
2019-06-06 12:39:21 -0700

    6/6/19 - Changed main function to accomodate new data pipeline of reading all CSV files from disk without having to specify filename.
    Mercury 1 Complete!

--- main.py
+++ main.py
@@ -4,34 +4,28 @@ import numpy as np
 from data_loader.data_loader import DataLoader
 from models.dense_model import DenseModel
 from trainer.trainer import Trainer
+from os import listdir
+from os.path import isfile, join
 
 
 #GLOBAL VARIABLES
-_AAPL = "D:\\Dropbox\\9. Data\\Mercury Data\\CSV\\CIQ_AAPL.csv"
-_CMCSA = "D:\\Dropbox\\9. Data\\Mercury Data\\CSV\\CIQ_CMCSA.csv"
-_AMD = "D:\\Dropbox\\9. Data\\Mercury Data\\CSV\\CIQ_AMD.csv"
+mypath = "D:\\Dropbox\\9. Data\\Mercury Data\\CSV"
 
 
-def main(load=0):
+def main(data,load=0):
 
-    X_train = np.concatenate((data_aapl.X_train_std,
-                              data_cmcs.X_train_std,
-                              data_amd.X_train_std),
-                             axis=0)
-    Y_train = np.concatenate((data_aapl.Y_train,
-                              data_cmcs.Y_train,
-                              data_amd.Y_train),
-                             axis=0)
+    X_train = data[0].X_train_std
+    Y_train = data[0].Y_train
+    X_test = data[0].X_test_std
+    Y_test = data[0].Y_test
+
+    for i in range(1,len(data)):
+        X_train = np.concatenate((X_train,data[i].X_train_std), axis=0)
+        Y_train = np.concatenate((Y_train,data[i].Y_train), axis = 0)
+        X_test = np.concatenate((X_test,data[i].X_test_std), axis = 0)
+        Y_test = np.concatenate((Y_test,data[i].Y_test), axis = 0)
+
     num_features = X_train.shape[1]
-
-    X_test = np.concatenate((data_aapl.X_test_std,
-                             data_cmcs.X_test_std,
-                            data_amd.X_test_std),
-                            axis=0)
-    Y_test = np.concatenate((data_aapl.Y_test,
-                             data_cmcs.Y_test,
-                             data_amd.Y_test),
-                            axis=0)
 
     logging.info('Size of X_Train: %s',X_train.shape)
     logging.info('Size of X_Train: %s', Y_train.shape)
@@ -39,50 +33,51 @@ def main(load=0):
 
     dense_model = DenseModel(num_features)
 
+    # load model from h5 file
     if load == 1:
-        #load model from h5 file
         dense_model.load(".\saved_models\\Mercury 1.h5")
         results = dense_model.model.evaluate(X_test,Y_test,batch_size=32)
         print('test loss, test acc:',results)
 
+    # build and train and save model
     else:
-        #build model
         print('Create the model.')
         dense_model.build_model()
 
-        #train model
         print('Create the trainer')
-        trainer = Trainer(dense_model.model,X_train,Y_train,epochs=20,batch_size=32)
+        trainer = Trainer(dense_model.model,X_train,Y_train,epochs=100,batch_size=32)
+
         print('Start training the model.')
         trainer.train()
 
-        #save model
         dense_model.save(".\saved_models\\Mercury 1.h5")
 
 
+def combineData(mypath):
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+    data_dict = {}
+    arr = []
+
+    for f in onlyfiles:
+        key = f.replace('.csv', '')
+        data_dict.update({key: mypath + "\\" + f})
+
+    for k in data_dict:
+        fname = data_dict[k]
+        arr.append(DataLoader(fname,
+                              window=10,
+                              threshold=0.03,
+                              technicals=0,
+                              featselect=0,
+                              drop=0))
+    return arr
+
+
 if __name__ == '__main__':
-    data_aapl = DataLoader(_AAPL,
-                      window=10,
-                      threshold=0.03,
-                      technicals=0,
-                      featselect=0,
-                      drop=0)
 
-    data_cmcs = DataLoader(_CMCSA,
-                      window=10,
-                      threshold=0.03,
-                      technicals=0,
-                      featselect=0,
-                      drop=0)
-
-    data_amd = DataLoader(_AMD,
-                      window=10,
-                      threshold=0.03,
-                      technicals=0,
-                      featselect=0,
-                      drop=0)
-
-    main(load=0)
+    data = combineData(mypath)
+    main(data,load=0)
     logging.info('Successful execution')
-    # os.system("tensorboard --logdir=.\\logs\\")
+    os.system("tensorboard --logdir=.\\logs\\")
 
